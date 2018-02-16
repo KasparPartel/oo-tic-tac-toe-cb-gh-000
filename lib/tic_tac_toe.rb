82ef596eead7e68ae2011421b233dde55e897b62
Kaspar JohannesPärtel <kasparjohannes.partel@tptlive.ee>
2018-02-16 14:16:29 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -71,4 +71,14 @@ def turn_count
   return counter
 end
 
+def current_player
+  if turn_count == 0
+    return 'X'
+  elsif turn_count % 2 == 0
+    return 'X'
+  else
+    return 'O'
+  end
+end
+
 end
